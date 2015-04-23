7585cf72dc4774f43c9f30385c63acb2fcf27a98
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-23 14:53:55 -0700

    #1 add ifmain block

--- update.py
+++ update.py
@@ -9,43 +9,48 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 REGION = 'us-west-2'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 BUCKET = 'landsat-pds'
-NEW_SCENE_LIST_GZ = 'scene_list.gz'
+NEW_SCENE_LIST = 'scene_list.gz'
 OLD_SCENE_LIST = 'scene_list'
 
-# Make connection to S3
-S3conn = connect_to_region(REGION)
 
-# Get landsat-pds bucket
-landsat_pds_bucket = S3conn.get_bucket(BUCKET)
+def main():
+    # Make connection to S3
+    S3conn = connect_to_region(REGION)
 
-# Get new scene list file key
-scene_list_key = landsat_pds_bucket.get_key(NEW_SCENE_LIST)
+    # Get landsat-pds bucket
+    landsat_pds_bucket = S3conn.get_bucket(BUCKET)
 
-# Get new scene list file
-new_scene_list_name = 'scene_list_{}'.format(date.today().strftime('%Y%m%d'))
-scene_list_key.get_contents_to_filename('{}.gz'.format(new_scene_list_name))
+    # Get new scene list file key
+    scene_list_key = landsat_pds_bucket.get_key(NEW_SCENE_LIST)
 
-# Load new scene list file
-new_scene_list_file_object = gzip.open(new_scene_list_name)
-new_scene_list = new_scene_list_file_object.readlines()
+    # Get new scene list file
+    new_scene_list_name = 'scene_list_{}'.format(date.today().strftime('%Y%m%d'))
+    scene_list_key.get_contents_to_filename('{}.gz'.format(new_scene_list_name))
 
-# Load old scene list file
-old_scene_list_file_object = open(OLD_SCENE_LIST, 'rb')
-old_scene_list = old_scene_list_file_object.readlines()
+    # Load new scene list file
+    new_scene_list_file_object = gzip.open(new_scene_list_name)
+    new_scene_list = new_scene_list_file_object.readlines()
 
-# Caluculate diffrence between old and new scene lists
-diff = list(set(new_scene_list) - set(old_scene_list))
+    # Load old scene list file
+    old_scene_list_file_object = open(OLD_SCENE_LIST, 'rb')
+    old_scene_list = old_scene_list_file_object.readlines()
 
-# Write diff to csv file
-with open('diff.csv', 'wb') as d:
-    d.writelines(diff)
+    # Caluculate diffrence between old and new scene lists
+    diff = list(set(new_scene_list) - set(old_scene_list))
 
-# Close scene list files
-new_scene_list_file_object.close()
-old_scene_list_file_object.close()
+    # Write diff to csv file
+    with open('diff.csv', 'wb') as d:
+        d.writelines(diff)
 
-# Remove old scene list file
-os.remove(OLD_SCENE_LIST)
+    # Close scene list files
+    new_scene_list_file_object.close()
+    old_scene_list_file_object.close()
 
-# Rename new scene list
-os.rename(new_scene_list_name, OLD_SCENE_LIST)
+    # Remove old scene list file
+    os.remove(OLD_SCENE_LIST)
+
+    # Rename new scene list
+    os.rename(new_scene_list_name, OLD_SCENE_LIST)
+
+if __name__ == '__main__':
+    main()
