e2fe908ee29114abdde9a99f08d63e6acc48920c
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-23 13:15:46 -0700

    #1 add AWS credentials retrieval and database url

--- update.py
+++ update.py
@@ -1,3 +1,11 @@
+import boto
+
+# Define AWS credentials
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+REGION = 'us-west-2'
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
 download new scene list
 gunzip  new scene list
 sort -n new scene list
