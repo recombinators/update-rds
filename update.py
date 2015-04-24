3dbe407f1bd65092b2654c555bb0fc128d019a5d
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-24 13:58:31 -0700

    created seperate funcitons for all actions

--- update.py
+++ update.py
@@ -5,44 +5,22 @@ import os
 import psycopg2
 
 # Constants
-CREDENTIALS = 'credentials'
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+CREDENTIALS = FILE_PATH + '/' + 'credentials'
 REGION = 'us-west-2'
 BUCKET = 'landsat-pds'
 NEW_SCENE_LIST = 'scene_list.gz'
-OLD_SCENE_LIST = 'scene_list'
-DIFF = 'diff.csv'
+OLD_SCENE_LIST = FILE_PATH + '/' + 'scene_list'
+NEW_SCENE_LIST_NAME = 'scene_list_{}'.format(date.today().strftime('%Y%m%d'))
+NEW_SCENE_LIST_NAME_GZ = '{}.gz'.format(NEW_SCENE_LIST_NAME)
+DIFF = FILE_PATH + '/' + 'diff.csv'
 TABLE = 'path_row'
 SEP = ','
 
 
-def diff_to_db(DATABASE_URL, dif):
-    # Connect to DB
-    conn = psycopg2.connect(DATABASE_URL)
-
-    # Create DB cursor
-    cur = conn.cursor()
-
-    # Copy diff to DB
-    cur.copy_from(dif, TABLE, sep=SEP)
-
-    # Commit changes
-    conn.commit()
-
-    # Close communication with the database
-    cur.close()
-    conn.close()
-
-
-def write_to_update_log():
-    pass
-
-
-def main():
-    # Get current directory
-    file_path = os.path.dirname(os.path.realpath(__file__))
-
+def get_credentials():
     # Get credentials
-    with open(file_path + '/' + CREDENTIALS, 'rb') as cred:
+    with open(CREDENTIALS, 'rb') as cred:
         creds = cred.readlines()
 
     # Assign credentials
@@ -50,6 +28,10 @@ def main():
     AWS_SECRET_ACCESS_KEY = creds[1].rstrip('\n')
     DATABASE_URL = creds[2].rstrip('\n')
 
+    return AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, DATABASE_URL
+
+
+def get_new_scene_list(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
     # Make connection to S3
     S3conn = connect_to_region(REGION,
                                aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -62,12 +44,12 @@ def main():
     scene_list_key = landsat_pds_bucket.get_key(NEW_SCENE_LIST)
 
     # Get new scene list file
-    new_scene_list_name = 'scene_list_{}'.format(date.today().strftime('%Y%m%d'))
-    new_scene_list_name_gz = '{}.gz'.format(new_scene_list_name)
-    scene_list_key.get_contents_to_filename(new_scene_list_name_gz)
+    scene_list_key.get_contents_to_filename(NEW_SCENE_LIST_NAME_GZ)
 
-    with gzip.open(new_scene_list_name_gz) as new_scene_list_file_object, \
-            open(file_path + '/' + OLD_SCENE_LIST, 'rb') as old_scene_list_file_object:
+
+def create_diff():
+    with gzip.open(NEW_SCENE_LIST_NAME_GZ) as new_scene_list_file_object, \
+            open(OLD_SCENE_LIST, 'rb') as old_scene_list_file_object:
 
         # Load new scene list file
         new_scene_list = new_scene_list_file_object.readlines()
@@ -79,19 +61,54 @@ def main():
         diff = list(set(new_scene_list) - set(old_scene_list))
 
     # Write diff to csv file
-    with open(file_path + '/' + DIFF, 'wb') as dif:
+    with open(DIFF, 'wb') as dif:
         dif.writelines(diff)
 
+    return dif, new_scene_list
+
+
+def diff_to_db(DATABASE_URL, dif):
+    with open(DIFF, 'rb') as dif:
+        # Connect to DB
+        conn = psycopg2.connect(DATABASE_URL)
+
+        # Create DB cursor
+        cur = conn.cursor()
+
+        # Copy diff to DB
+        cur.copy_from(dif, TABLE, sep=SEP)
+
+        # Commit changes
+        conn.commit()
+
+        # Close communication with the database
+        cur.close()
+        conn.close()
+
+
+def write_to_update_log():
+    pass
+
+
+def main():
+    # Get credentials
+    AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, DATABASE_URL = get_credentials()
+
+    # Get new scene list
+    get_new_scene_list(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+
+    # Create diff file
+    dif, new_scene_list = create_diff()
+
     # Write diff to db
-    with open(file_path + '/' + DIFF, 'rb') as dif:
-        diff_to_db(DATABASE_URL, dif)
+    diff_to_db(DATABASE_URL, dif)
 
     # Overwrite scene list file with new data
-    with open(file_path + '/' + OLD_SCENE_LIST, 'wb') as old:
+    with open(OLD_SCENE_LIST, 'wb') as old:
         old.writelines(new_scene_list)
 
     # Remove new scene list file
-    os.remove(new_scene_list_name_gz)
+    os.remove(NEW_SCENE_LIST_NAME_GZ)
 
 if __name__ == '__main__':
     main()
